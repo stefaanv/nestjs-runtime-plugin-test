3143e823dc624a721a2913e463cb08d38b305204
Stefaan Vandevelde <stefaan.vandevelde@bruyland.be>
2023-11-22 12:17:43 +0100

    config test conclusive

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class AppService {
+  constructor(private readonly _config: ConfigService) {
+    console.log(_config.get('test'))
+  }
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!'
   }
 }
